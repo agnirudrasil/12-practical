b09e61208d97179b9a382ce1db80e95173316ed5
Agnirudra Sil <agnirudra.sil@gmail.com>
2021-09-14 14:29:59 +0530

    Question 12 complete

---
+++ src/question_12/main.py
@@ -0,0 +1,34 @@
+"""
+Write a program to create a binary file called emp.dat and write into it the employee details of in the form
+of dictionaries. Write the function to increase the Salary of the employee by 5000 for the empNo “123”.
+For Ex. D= {empNo:"123", ename:"Ajay”, Salary: 50000}
+"""
+
+import pickle
+from random import choice, randint
+
+names = ["Ram", "Ramesh", "Rohan", "Sai",
+         "Jack", "Joe", "Oliver", "Robert", "Noah"]
+
+
+"""
+Create file emp.dat
+"""
+with open("emp.dat", "wb") as f:
+    ch = randint(0, 9)
+    pickle.dump([{"empNo": "123" if i == "ch" else str(randint(100, 999)),
+                  "ename": "Ajay" if i == ch else choice(names),
+                  "Salary": randint(10000, 99999)}
+                for i in range(10)], f)
+
+try:
+    with open("emp.dat", "rb+") as f:
+        while True:
+            rpos = f.tell()
+            data = pickle.load(f)
+            if data["empNo"] == "123":
+                data['Salary'] += 5000
+                f.seek(rpos)
+                pickle.dump(data, f)
+except EOFError:
+    print("")
